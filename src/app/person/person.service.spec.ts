7d032aa3c855c26f9b3834396904b0405d0538dd
Alexandre TRUCCHIERO <>
2023-09-30 10:49:41 +0200

    👔✅ ATO - 2.Dans la zone de génération, des critères de sélection sont affichés, implémenter le choix de ces critères pour la génération (valider la génération au moyen des tests unitaires) : Update getPersons

--- src/app/person/person.service.spec.ts
+++ src/app/person/person.service.spec.ts
@@ -3,6 +3,7 @@ import {Person} from "./person";
 import {PersonListComponent} from "./person-list/person-list.component";
 import {PersonService} from "./person.service";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {GenderEnum} from "../shared/enums/gender.enum";
 
 const PERSONS: Person[] = [
   {
@@ -58,18 +59,66 @@ describe("PersonService", () => {
     expect(component).toBeDefined();
   });
 
-  test("should provide a list of 3 persons", fakeAsync(() => {
-
-    expect(personService.getPersons).toBeTruthy();
-
-    personService.getPersons(DEFAULT_CONFIG).subscribe();
-    httpClientMock.expectOne("/assets/data/persons.json", 'GET').flush(PERSONS);
-
+  test("given PERSONS dataset with DEFAULT_CONFIG when getPersons then return 3 persons", fakeAsync(() => {
+    //given
+    //when
     personService.getPersons(DEFAULT_CONFIG).subscribe(element => {
-      expect(element.length).toBe(1);
+      // then
+      expect(element.length).toBe(3);
       expect(element).toEqual(PERSONS);
     });
+    httpClientMock.expectOne("/assets/data/persons.json", 'GET').flush(PERSONS);
+    httpClientMock.verify();
+  }));
 
+  test("given PERSONS dataset with config count 1 when getPersons then return 1 person", fakeAsync(() => {
+    // given
+    const config = {
+      count: 1,
+      male: true,
+      female: true,
+    };
+    // when
+    personService.getPersons(config).subscribe(element => {
+      // then
+      expect(element.length).toBe(1);
+    });
+    httpClientMock.expectOne("/assets/data/persons.json", 'GET').flush(PERSONS);
+    httpClientMock.verify();
+  }));
+
+  test("given PERSONS dataset with config male false when getPersons then return 1 person female", fakeAsync(() => {
+    // given
+    const config = {
+      count: 3,
+      male: false,
+      female: true,
+    };
+    // when
+    personService.getPersons(config).subscribe(element => {
+      // then
+      expect(element.length).toBe(1);
+      element.forEach((elem) => expect(elem.gender).toBe(GenderEnum.FEMALE))
+    });
+    httpClientMock.expectOne("/assets/data/persons.json", 'GET').flush(PERSONS);
+    httpClientMock.verify();
+  }));
+
+  test("given PERSONS dataset with config female false when getPersons then return 2 persons male", fakeAsync(() => {
+    // given
+    const config = {
+      count: 3,
+      male: true,
+      female: false,
+    };
+    // when
+    personService.getPersons(config).subscribe(element => {
+      // then
+      expect(element.length).toBe(2);
+      element.forEach((elem) => expect(elem.gender).toBe(GenderEnum.MALE))
+    });
+    httpClientMock.expectOne("/assets/data/persons.json", 'GET').flush(PERSONS);
+    httpClientMock.verify();
   }));
 
 });
--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -1,8 +1,9 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {GenerationConfig} from "./generation-config";
 import {Person} from "./person";
+import {GenderEnum} from "../shared/enums/gender.enum";
 
 @Injectable({
   providedIn: "root"
@@ -13,6 +14,22 @@ export class PersonService {
   }
 
   getPersons(config: GenerationConfig): Observable<Person[]> {
-    return this.http.get<Person[]>("/assets/data/persons.json");
+    return this.http.get<Person[]>("/assets/data/persons.json")
+      .pipe(
+        map((response: Person[]) => {
+          // TODO : Move the filters to the backend for better performance
+          response = this.filterGender(response, config.male, GenderEnum.MALE);
+          response = this.filterGender(response, config.female, GenderEnum.FEMALE);
+          return this.filterCount(response, config.count);
+        })
+      );
+  }
+
+  private filterGender(response: Person[], keepGender: boolean, gender: GenderEnum): Person[] {
+    return response.filter((response: Person) => (!keepGender) ? response.gender != gender : true);
+  }
+
+  private filterCount(response: Person[], count: number): Person[] {
+    return response.slice(0, count)
   }
 }
